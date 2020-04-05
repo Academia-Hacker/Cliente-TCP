7b7ae7f9bc428a2c9357d820c9d4ae34bb4665ad
everttonbs <ebs@ic.ufal.br>
2020-04-05 20:23:18 -0300

    Update

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -9,12 +9,17 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #faz o cliente se conectar
 client.connect((target_host, target_port))
-'''
+
 #envia alguns dados
-client.send(b"GET /HTTP/1.1\r\nHost: google.com\r\n\r\n")
+#client.send(b"GET /HTTP/1.1\r\nHost: google.com\r\n\r\n")
+msg = 'Hello, World! Eu sou o cliente'
+client.send(msg.encode())
 
 #recebe alguns dados
 response = client.recv(4096)
 
 print(response)
-'''
+
+
+
+
