a297566e30c602645beb75c275727cf0acc60b4b
everttonbs <ebs@ic.ufal.br>
2020-04-04 23:51:12 -0300

    Update

--- cliente_tcp.py
+++ cliente_tcp.py
@@ -1,15 +1,15 @@
 
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "localhost"
+target_port = 9999
 
 #cria um objeto socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #faz o cliente se conectar
 client.connect((target_host, target_port))
-
+'''
 #envia alguns dados
 client.send(b"GET /HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
@@ -17,3 +17,4 @@ client.send(b"GET /HTTP/1.1\r\nHost: google.com\r\n\r\n")
 response = client.recv(4096)
 
 print(response)
+'''
